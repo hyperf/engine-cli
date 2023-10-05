fe0d9a898bd72e715611d97d6e13407345b4da04
李铭昕 <715557344@qq.com>
2023-10-05 23:54:12 +0800

    Added test cases about DepthGuard.

--- tests/Cases/DiTest.php
+++ tests/Cases/DiTest.php
@@ -12,13 +12,35 @@ declare(strict_types=1);
 
 namespace HyperfTest\Cases;
 
+use Hyperf\Context\Context;
+use Hyperf\Di\Exception\CircularDependencyException;
+use Hyperf\Di\Resolver\DepthGuard;
+
 /**
  * @internal
  * @coversNothing
  */
 class DiTest extends AbstractTestCase
 {
+    protected function tearDown(): void
+    {
+        Context::destroy('di.depth');
+    }
+
     public function testDepthGuard()
     {
+        $guard = DepthGuard::getInstance();
+        $guard->increment();
+        $depth = Context::get('di.depth');
+        $this->assertSame(1, $depth);
+    }
+
+    public function testDepthGuardException()
+    {
+        $guard = DepthGuard::getInstance();
+        $this->expectException(CircularDependencyException::class);
+        for ($i = 0; $i < 501; ++$i) {
+            $guard->increment();
+        }
     }
 }
